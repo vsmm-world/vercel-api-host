984f85622f627ddbe2aa30b58d5ef9840d4135a5
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2025-02-22 22:52:50 +0530

    feat: add new controller method to fetch data from external API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,24 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from 'axios';
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  getControlller() {
+    return axios
+      .get('https://api.portglint.co')
+      .then((response) => {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  @Get('api')
   getHello() {
     return axios
       .get('https://api.portglint.co')
