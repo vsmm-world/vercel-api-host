c816c70c339a9e445146eb190c3828bc730cddae
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2025-02-22 22:50:50 +0530

    fix: return API response data in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,7 @@ export class AppController {
       .get('https://api.portglint.co')
       .then((response) => {
         console.log(response.data);
+        return response.data;
       })
       .catch((error) => {
         console.error(error);
